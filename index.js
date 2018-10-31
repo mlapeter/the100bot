aa33a55d92855b2eb2d8ae2e3e37d5ca6119632e
mlapeter <mlapeter@gmail.com>
2018-10-31 12:12:22 -0700

    playing around with test commands

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const { CommandoClient } = require("discord.js-commando");
+const path = require("path");
+
+// const Discord = require("discord.js");
+// const client = new Discord.Client();
+
+const client = new CommandoClient({
+  commandPrefix: "!",
+  owner: "126736943311486976",
+  invite: "https://discord.gg/dBZRVB9",
+  disableEveryone: true
+});
+
+client.registry
+  .registerDefaultTypes()
+  .registerGroups([
+    ["general", "General Commands"],
+    ["second", "Your Second Command Group"]
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  .registerCommandsIn(path.join(__dirname, "commands"));
 
 client.on("ready", () => {
-  console.log("Ready!");
+  console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+  client.user.setActivity("with Commando");
 });
-
-client.on("message", message => {
-  if (message.content === "!ping") {
-    message.channel.send("Pong from discordjs!");
-  }
-});
-
+client.on("error", console.error);
 client.login(process.env.DISCORD_BOT_TOKEN);
