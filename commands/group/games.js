d46d9732736581fb7ad06e0fb64fcac644a599e0
mlapeter <mlapeter@gmail.com>
2018-11-02 14:39:33 -0700

    add api token header to gaming sessions command

--- commands/group/games.js
+++ commands/group/games.js
@@ -21,10 +21,20 @@ module.exports = class GamesCommand extends Command {
       msg.guild.id
     }`;
     console.log(content);
-    const res = await fetch(link, { method: "POST" });
+    const res = await fetch(link, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + process.env.THE100_API_TOKEN
+      }
+    });
+    console.log(res.status);
+    if (res.status !== 201) {
+      return msg.say(
+        "Not Authorized - make sure the bot creator is using the correct API Token."
+      );
+    }
     const json = await res.json();
     console.log(json);
-
-    // return msg.say(`this will post to: ${link}`);
   }
 };
