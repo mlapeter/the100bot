ac33509950cf8a521b617505cb3a833ec3659919
mlapeter <mlapeter@gmail.com>
2018-11-02 15:17:32 -0700

    fix typo in linking url

--- commands/general/link.js
+++ commands/general/link.js
@@ -17,7 +17,7 @@ module.exports = class LinkCommand extends Command {
   run(msg) {
     let content = `Click here to link your account: https://the100.io/linkdiscord/${
       msg.author.username
-    }/#${msg.author.discriminator}`;
+    }/${msg.author.discriminator}`;
     console.log(content);
     msg.react("💯");
     return msg.author.send(content);
