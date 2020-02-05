95696b67c61591b40ed65e3ba8b69df06078b897
mlapeter <mlapeter@gmail.com>
2020-02-05 11:19:53 -0800

    cleanup

--- utils/api.js
+++ utils/api.js
@@ -37,9 +37,6 @@ module.exports = class Api {
 
     const res = await this.post(url, data)
 
-    console.log("********** api.js *******************")
-    console.log(res)
-
     if (res.status !== 201) {
       return msg.say(
         "Not Authorized - make sure the bot creator is using the correct API Token."
