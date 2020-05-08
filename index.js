3ac360a4e0fccdc707a1a7b2d8a73962146f41d6
mlapeter <mlapeter@gmail.com>
2020-05-08 10:36:46 -0700

    try pg rejectUnauthorized to fix self signed cert error

--- index.js
+++ index.js
@@ -50,6 +50,12 @@ console.log("DB URL: ")
 console.log(process.env.DATABASE_URL)
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+    }
+  },
   // port: 5432,
   // dialect: 'postgres',
   // native: true
