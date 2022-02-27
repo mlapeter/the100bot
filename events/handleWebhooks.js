e3725febcd8782765cdc82889be871e87c1146b3
mlapeter <mlapeter@gmail.com>
2022-02-27 15:25:57 -0700

    add catch statements

--- events/handleWebhooks.js
+++ events/handleWebhooks.js
@@ -7,89 +7,93 @@ const discordApi = new DiscordApi();
 // listen for webhooks received from the100.io and log to console
 module.exports = (client) => {
   client.on("message", async (message) => {
-    if (message.webhookID) {
-      console.log("WEBHOOK RECEIVED");
-      console.log(message.content);
-      // if message.content includes 'jump to' then we link to existing embed and update it
-      if (message.content.includes("jump to")) {
-        console.log("JUMP TO RECEIVED");
-        // parse out the url in the message
-        const urlRegex = /(https?:\/\/[^\s]+)/g;
-        const url = message.content.match(urlRegex)[0];
-        // parse out the string after the very last slash in the url
-        const idRegex = /\/([^\/]+)$/;
-        const id = url.match(idRegex)[1];
-        // strip out ) from the id
-        const idStripped = id.replace(")", "");
-        console.log(idStripped);
+    try {
+      if (message.webhookID) {
+        console.log("WEBHOOK RECEIVED");
+        console.log(message.content);
+        // if message.content includes 'jump to' then we link to existing embed and update it
+        if (message.content.includes("jump to")) {
+          console.log("JUMP TO RECEIVED");
+          // parse out the url in the message
+          const urlRegex = /(https?:\/\/[^\s]+)/g;
+          const url = message.content.match(urlRegex)[0];
+          // parse out the string after the very last slash in the url
+          const idRegex = /\/([^\/]+)$/;
+          const id = url.match(idRegex)[1];
+          // strip out ) from the id
+          const idStripped = id.replace(")", "");
+          console.log(idStripped);
 
-        // find the message in the channel with the id
-        const msg = await message.channel.messages.fetch(idStripped);
+          // find the message in the channel with the id
+          const msg = await message.channel.messages.fetch(idStripped);
 
-        // wait 2 seconds to allow the message to be fetched
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+          // wait 2 seconds to allow the message to be fetched
+          await new Promise((resolve) => setTimeout(resolve, 2000));
 
-        const json = await api.postReaction({
-          action: "refresh_gaming_session",
-          msg: msg,
-          user: msg.author,
-          body: {},
-        });
-        const { notice, gaming_session } = json;
+          const json = await api.postReaction({
+            action: "refresh_gaming_session",
+            msg: msg,
+            user: msg.author,
+            body: {},
+          });
+          const { notice, gaming_session } = json;
 
-        if (!gaming_session) {
-          console.log("ERROR:");
-          console.log(json.notice);
-          return;
+          if (!gaming_session) {
+            console.log("ERROR:");
+            console.log(json.notice);
+            return;
+          }
+
+          console.log("GAMING SESSION:");
+          console.log(gaming_session);
+
+          const receivedEmbed = msg.embeds[0];
+          console.log("EXISTING EMBED:");
+          console.log(receivedEmbed);
+
+          const newEmbed = await discordApi.embedGamingSessionDynamic(gaming_session, receivedEmbed);
+          msg.edit({ embed: newEmbed });
+        } else {
+          // Otherwise, we create a new embed
+          console.log("CREATING NEW EMBED...");
+          // get the message's embed
+          const receivedEmbed = message.embeds[0];
+          if (!receivedEmbed) {
+            console.log("NO EMBED FOUND");
+            return;
+          }
+
+          const messageContent = message.content;
+          console.log("RECEIVED EMBED:");
+          console.log(receivedEmbed);
+          await message.delete();
+
+          // for the string receivedEmbed.url, parse out the id
+          const idRegex = /\/([^\/]+)$/;
+          const id = receivedEmbed.url.match(idRegex)[1];
+          const gamingSessionId = id.split("?")[0];
+
+          console.log("ID:");
+          console.log(gamingSessionId);
+
+          const newEmbed = await discordApi.convertEmbedToGamingSessionWithReactions(receivedEmbed);
+
+          // post a new message with the messageContent
+          const newMessage = await message.channel.send(messageContent, { embed: newEmbed });
+          await newMessage.react("✅");
+          await newMessage.react("📝");
+          await api.postAction({
+            action: "update_gaming_session",
+            msg: newMessage,
+            body: {
+              gaming_session_id: gamingSessionId,
+              embed_id: newMessage.id,
+            },
+          });
         }
-
-        console.log("GAMING SESSION:");
-        console.log(gaming_session);
-
-        const receivedEmbed = msg.embeds[0];
-        console.log("EXISTING EMBED:");
-        console.log(receivedEmbed);
-
-        const newEmbed = await discordApi.embedGamingSessionDynamic(gaming_session, receivedEmbed);
-        msg.edit({ embed: newEmbed });
-      } else {
-        // Otherwise, we create a new embed
-        console.log("CREATING NEW EMBED...");
-        // get the message's embed
-        const receivedEmbed = message.embeds[0];
-        if (!receivedEmbed) {
-          console.log("NO EMBED FOUND");
-          return;
-        }
-
-        const messageContent = message.content;
-        console.log("RECEIVED EMBED:");
-        console.log(receivedEmbed);
-        await message.delete();
-
-        // for the string receivedEmbed.url, parse out the id
-        const idRegex = /\/([^\/]+)$/;
-        const id = receivedEmbed.url.match(idRegex)[1];
-        const gamingSessionId = id.split("?")[0];
-
-        console.log("ID:");
-        console.log(gamingSessionId);
-
-        const newEmbed = await discordApi.convertEmbedToGamingSessionWithReactions(receivedEmbed);
-
-        // post a new message with the messageContent
-        const newMessage = await message.channel.send(messageContent, { embed: newEmbed });
-        await newMessage.react("✅");
-        await newMessage.react("📝");
-        await api.postAction({
-          action: "update_gaming_session",
-          msg: newMessage,
-          body: {
-            gaming_session_id: gamingSessionId,
-            embed_id: newMessage.id,
-          },
-        });
       }
+    } catch (e) {
+      console.log(e);
     }
   });
 };
--- events/messageReactionRemove.js
+++ events/messageReactionRemove.js
@@ -7,48 +7,52 @@ const discordApi = new DiscordApi();
 
 module.exports = (client) => {
   client.on("messageReactionRemove", async (reaction, user) => {
-    if (user.bot) return;
+    try {
+      if (user.bot) return;
 
-    if (reaction.partial) {
-      // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
-      try {
-        await reaction.fetch();
-      } catch (error) {
-        console.error("Something went wrong when fetching the message:", error);
-        return;
-      }
-    }
-
-    console.log(`${reaction.message.author}'s message "${reaction.message.content}" lost a reaction!`);
-
-    if (reaction.emoji.name === "✅") {
-      console.log("user leaving game");
-      const json = await api.postReaction({
-        action: "leave_gaming_session",
-        msg: reaction.message,
-        user: user,
-        body: {},
-      });
-      console.log(json);
-      const { notice, gaming_session } = json;
-
-      if (!notice) {
-        console.log("ERROR in messageReactionRemove");
+      if (reaction.partial) {
+        // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+        try {
+          await reaction.fetch();
+        } catch (error) {
+          console.error("Something went wrong when fetching the message:", error);
+          return;
+        }
       }
 
-      if (!gaming_session) {
-        return;
+      console.log(`${reaction.message.author}'s message "${reaction.message.content}" lost a reaction!`);
+
+      if (reaction.emoji.name === "✅") {
+        console.log("user leaving game");
+        const json = await api.postReaction({
+          action: "leave_gaming_session",
+          msg: reaction.message,
+          user: user,
+          body: {},
+        });
+        console.log(json);
+        const { notice, gaming_session } = json;
+
+        if (!notice) {
+          console.log("ERROR in messageReactionRemove");
+        }
+
+        if (!gaming_session) {
+          return;
+        }
+
+        console.log("GAMING SESSION: ");
+        console.log(gaming_session);
+
+        const receivedEmbed = reaction.message.embeds[0];
+
+        const exampleEmbed = await discordApi.embedGamingSessionDynamic(gaming_session, receivedEmbed);
+
+        // reaction.message.channel.send({ embed: exampleEmbed });
+        reaction.message.edit({ embed: exampleEmbed });
       }
-
-      console.log("GAMING SESSION: ");
-      console.log(gaming_session);
-
-      const receivedEmbed = reaction.message.embeds[0];
-
-      const exampleEmbed = await discordApi.embedGamingSessionDynamic(gaming_session, receivedEmbed);
-
-      // reaction.message.channel.send({ embed: exampleEmbed });
-      reaction.message.edit({ embed: exampleEmbed });
+    } catch (e) {
+      console.log(e);
     }
   });
 };
